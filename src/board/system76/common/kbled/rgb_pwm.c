0d224736a23536e9fbb4304b0e0bc8a8d9d83f0b
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-06-01 18:10:22 +0200

    board/system76/common/kbled: enable KBD backlight on reset if lid open
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/board/system76/common/kbled/rgb_pwm.c
+++ src/board/system76/common/kbled/rgb_pwm.c
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <board/gpio.h>
 #include <board/kbled.h>
 #include <ec/pwm.h>
 
 void kbled_init(void) {
     //TODO: enable PWMs
+
     kbled_reset();
 }
 
 void kbled_reset(void) {
     // Set brightness and color
+    kbled_set_brightness(0);
+    if (gpio_get(&LID_SW_N))
+        kbled_enable(true);
     kbled_set_color(0xFFFFFF);
-    kbled_set(0x00);
 }
 
 uint8_t kbled_get(void) {
